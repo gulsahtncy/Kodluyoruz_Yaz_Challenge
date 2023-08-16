e94461f5b7716b1a2dfa07afaf798a38f3a949f6
Gülşah <gulsahtuncay1@gmail.com>
2023-08-16 22:09:55 +0300

    Kodluyoruz-Yaz-Challenge

--- week3/Easy.java
+++ week3/Easy.java
@@ -1,5 +1,12 @@
 package week3;
 
 public class Easy {
+	public static void main(String[] args) {
+		int hiz = 60; // km/saat
+		int yol = 240; // km
 
+		int sure = yol / hiz;
+
+		System.out.println("Araba " + yol + " km yol almak için " + sure + " saat sürer.");
+	}
 }
