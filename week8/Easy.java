b953fcee0255ffe594ef233d6186f2aff5bdc36d
Gülşah <gulsahtuncay1@gmail.com>
2023-08-20 18:53:25 +0300

    Kodluyoruz-Yaz-Challenge

--- week8/Easy.java
+++ week8/Easy.java
@@ -1,5 +1,19 @@
 package week8;
 
-public class Easy {
+import java.util.Scanner;
 
+public class Easy {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Bir sayı girin: ");
+		double sayi = scanner.nextDouble();
+
+		double kare = sayi * sayi;
+
+		System.out.println(sayi + " sayısının karesi: " + kare);
+
+		scanner.close();
+
+	}
 }
