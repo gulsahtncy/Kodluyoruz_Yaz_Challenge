f9619dc3413f3aa61f5240d2e13ba779c95ef6a1
Gülşah <gulsahtuncay1@gmail.com>
2023-08-20 18:33:53 +0300

    Kodluyoruz-Yaz-Challenge

---
+++ week5/Easy.java
@@ -0,0 +1,19 @@
+package week5;
+
+import java.util.Scanner;
+
+public class Easy {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+        
+        if (sayi % 2 == 0) {
+            System.out.println(sayi + " çift bir sayıdır.");
+        } else {
+            System.out.println(sayi + " tek bir sayıdır.");
+        }        
+        scanner.close();
+    }
+}
