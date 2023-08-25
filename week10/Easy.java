7ec220dc2f1452acd54c705da40cb92419f49e95
Gülşah <gulsahtuncay1@gmail.com>
2023-08-25 12:33:04 +0300

    Kodluyoruz-Yaz-Challenge

--- week10/Easy.java
+++ week10/Easy.java
@@ -1,5 +1,14 @@
 package week10;
 
 public class Easy {
+	public static void main(String[] args) {
+		int puan3 = 3;
+		int puan2 = 2;
+		int isabetPuan3 = 5;
+		int isabetPuan2 = 10;
 
+		int toplamPuan = (puan3 * isabetPuan3) + (puan2 * isabetPuan2);
+
+		System.out.println("Toplam puan: " + toplamPuan);
+	}
 }
